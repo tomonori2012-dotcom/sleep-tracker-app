82c696eff7cc393c2e44e7d727bdfcdf7bb37a92
tomonori2012-dotcom <tomonori2012@gmail.com>
2026-08-19 23:01:16 +0900

    Fix data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO sleep_record (date, hours, memo) VALUES ('2026-08-10', 7.5, 'ぐっすり眠れた');
-INSERT INTO sleep_record (date, hours, memo) VALUES ('2026-08-11', 6.0, '少し夜更かしをした');
+-- INSERT INTO sleep_record (date, hours, memo) VALUES ('2026-08-10', 7.5, 'ぐっすり眠れた');
+-- INSERT INTO sleep_record (date, hours, memo) VALUES ('2026-08-11', 6.0, '少し夜更かしした');
